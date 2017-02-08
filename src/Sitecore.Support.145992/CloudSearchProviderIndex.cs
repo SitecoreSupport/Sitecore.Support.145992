5cd30445e1347908629e32a68170696915f03b3d
dk\ivd <ivd@sitecore.net>
2017-02-08 11:03:23 +0800

    #146322:  Workaround for issue 136614 has been added

---
+++ src/Sitecore.Support.145992/CloudSearchProviderIndex.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Sitecore.Support.ContentSearch.Azure
+{
+    using System.Reflection;
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Azure.Http;
+    using Sitecore.ContentSearch.Azure.Schema;
+    using Sitecore.ContentSearch.Maintenance;
+
+    public class CloudSearchProviderIndex : Sitecore.ContentSearch.Azure.CloudSearchProviderIndex
+    {
+        public CloudSearchProviderIndex(string name, string connectionStringName, string totalParallelServices,
+            IIndexPropertyStore propertyStore) : base(name, connectionStringName, totalParallelServices, propertyStore)
+        {
+        }
+
+        public CloudSearchProviderIndex(string name, string connectionStringName, string totalParallelServices,
+            IIndexPropertyStore propertyStore, string @group)
+            : base(name, connectionStringName, totalParallelServices, propertyStore, @group)
+        {
+        }
+
+        #region Workaround for issue 136614
+
+        public new ICloudSearchIndexSchemaBuilder SchemaBuilder
+        {
+            get { return (this as Sitecore.ContentSearch.Azure.CloudSearchProviderIndex).SchemaBuilder; }
+            set
+            {
+                var pi = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex)
+                    .GetProperty("SchemaBuilder", BindingFlags.Instance | BindingFlags.Public);
+                pi.SetValue(this, value);
+            }
+        }
+
+
+        public new ISearchService SearchService
+        {
+            get { return (this as Sitecore.ContentSearch.Azure.CloudSearchProviderIndex).SearchService; }
+            set
+            {
+                var pi = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex)
+                    .GetProperty("SearchService", BindingFlags.Instance | BindingFlags.Public);
+                pi.SetValue(this, value);
+            }
+        }
+
+        #endregion
+    }
+}
