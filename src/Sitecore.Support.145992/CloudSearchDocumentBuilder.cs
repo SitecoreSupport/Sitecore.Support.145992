7184c99416626d928611ffae6a9894d811d8ebd4
dk\ivd <ivd@sitecore.net>
2017-02-08 10:59:21 +0800

    #145992: FieldReaders are used to get values from fields

---
+++ src/Sitecore.Support.145992/CloudSearchDocumentBuilder.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Sitecore.Support.ContentSearch.Azure
+{
+    using Sitecore.ContentSearch;
+
+    public class CloudSearchDocumentBuilder : Sitecore.ContentSearch.Azure.CloudSearchDocumentBuilder
+    {
+        public CloudSearchDocumentBuilder(IIndexable indexable, IProviderUpdateContext context) : base(indexable, context)
+        {
+        }
+
+        public override void AddField(IIndexableDataField field)
+        {
+            var value = this.Index.Configuration.FieldReaders.GetFieldValue(field);
+
+            this.AddField(field.Name, value);
+        }
+
+    }
+}
