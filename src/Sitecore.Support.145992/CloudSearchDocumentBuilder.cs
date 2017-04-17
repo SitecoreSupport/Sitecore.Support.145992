b3e9d419ff3acebc42e3b79fe60cb95447e5801f
DK\psk <psk@sitecore.net>
2017-04-17 18:19:24 -0400

    Add support for full-text search on content field. Add support for appending field values. Refactoring.

--- src/Sitecore.Support.145992/CloudSearchDocumentBuilder.cs
+++ src/Sitecore.Support.145992/CloudSearchDocumentBuilder.cs
@@ -1,8 +1,10 @@
-﻿
-namespace Sitecore.Support.ContentSearch.Azure
+﻿namespace Sitecore.Support.ContentSearch.Azure
 {
     using Sitecore.ContentSearch;
-
+    using Sitecore.ContentSearch.Azure;
+    using System;
+    using Sitecore.ContentSearch.Diagnostics;
+    using System.Collections.Concurrent;
     public class CloudSearchDocumentBuilder : Sitecore.ContentSearch.Azure.CloudSearchDocumentBuilder
     {
         public CloudSearchDocumentBuilder(IIndexable indexable, IProviderUpdateContext context) : base(indexable, context)
@@ -13,8 +15,76 @@ namespace Sitecore.Support.ContentSearch.Azure
         {
             var value = this.Index.Configuration.FieldReaders.GetFieldValue(field);
 
-            this.AddField(field.Name, value);
+            if (!base.IsMedia && IndexOperationsHelper.IsTextField(field))
+            {
+                this.AddField(BuiltinFields.Content, value, true);
+            }
+            this.AddField(field.Name, value, false);
         }
 
+        public override void AddField(string fieldName, object fieldValue, bool append = false)
+        {
+            var cloudName = this.Index.FieldNameTranslator.GetIndexFieldName(fieldName);
+            if (!append && this.Document.ContainsKey(cloudName))
+            {
+                return;
+            }
+            var configuration = this.Index.Configuration.FieldMap.GetFieldConfiguration(fieldName) as CloudSearchFieldConfiguration;
+            if (configuration != null)
+            {
+                if (configuration.Ignore)
+                {
+                    return;
+                }
+
+                fieldValue = configuration.FormatForWriting(fieldValue);
+
+                if (fieldValue == null)
+                {
+                    VerboseLogging.CrawlingLogDebug(() => string.Format("Skipping field name:{0} - Value is empty.", fieldName));
+                    return;
+                }
+            }
+
+            var searchIndex = this.Index as CloudSearchProviderIndex;
+            if (searchIndex != null)
+            {
+                searchIndex.SchemaBuilder.AddField(cloudName, fieldValue);
+            }
+
+            var formattedValue = this.Index.Configuration.IndexFieldStorageValueFormatter.FormatValueForIndexStorage(fieldValue, cloudName);
+
+            if (append && base.Document.ContainsKey(cloudName) && formattedValue is string)
+            {
+                ConcurrentDictionary<string, object> document = base.Document;
+                document[cloudName] = document[cloudName] + " " + (string)formattedValue;
+                return;
+            }
+
+            if (!base.Document.ContainsKey(cloudName) && formattedValue != null)
+            {
+                this.Document.GetOrAdd(cloudName, formattedValue);
+            }
+        }
+
+        public override void AddComputedIndexFields()
+        {
+            foreach (var computedIndexField in this.Options.ComputedIndexFields)
+            {
+                object fieldValue;
+
+                try
+                {
+                    fieldValue = computedIndexField.ComputeFieldValue(this.Indexable);
+                }
+                catch (Exception ex)
+                {
+                    CrawlingLog.Log.Error(string.Format("Could not compute value for ComputedIndexField: {0} for indexable: {1}", computedIndexField.FieldName, this.Indexable.UniqueId), ex);
+                    continue;
+                }
+
+                this.AddField(computedIndexField.FieldName, fieldValue, true);
+            }
+        }
     }
 }
