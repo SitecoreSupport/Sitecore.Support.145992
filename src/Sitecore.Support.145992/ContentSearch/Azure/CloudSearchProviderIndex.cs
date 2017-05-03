13d1dd24c1e5396ed199a5be15be948b8aa84ee4
AlexSharamok <als@sitecore.net>
2017-05-03 16:34:38 +0300

    Added fix for #155653

---
+++ src/Sitecore.Support.145992/ContentSearch/Azure/CloudSearchProviderIndex.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Sitecore.Support.ContentSearch.Azure
+{
+  using System;
+  using System.Reflection;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Azure.Http;
+  using Sitecore.ContentSearch.Azure.Schema;
+  using Sitecore.ContentSearch.Maintenance;
+  using Sitecore.ContentSearch.Security;
+
+  public class CloudSearchProviderIndex : Sitecore.ContentSearch.Azure.CloudSearchProviderIndex
+  {
+    private ISearchResultsDeserializer deserializer;
+    private Action dEnsureInitialized;
+
+    public CloudSearchProviderIndex(string name, string connectionStringName, string totalParallelServices,
+        IIndexPropertyStore propertyStore) : base(name, connectionStringName, totalParallelServices, propertyStore)
+    {
+      this.InitializeDelegates();
+    }
+
+    public CloudSearchProviderIndex(string name, string connectionStringName, string totalParallelServices,
+        IIndexPropertyStore propertyStore, string @group)
+        : base(name, connectionStringName, totalParallelServices, propertyStore, @group)
+    {
+      this.InitializeDelegates();
+    }
+
+    public override void Initialize()
+    {
+      base.Initialize();
+      var d = typeof(CloudSearchProviderIndex).BaseType.GetField("deserializer", BindingFlags.Instance | BindingFlags.NonPublic);
+      this.deserializer = d as ISearchResultsDeserializer;
+    }
+
+    public override IIndexOperations Operations
+    {
+      get { return new Sitecore.Support.ContentSearch.Azure.CloudSearchIndexOperations(this); }
+    }
+
+    public override IProviderSearchContext CreateSearchContext(SearchSecurityOptions options)
+    {
+      this.dEnsureInitialized();
+      return new Sitecore.Support.ContentSearch.Azure.CloudSearchSearchContext(this, options);
+    }
+
+    public override IProviderUpdateContext CreateUpdateContext()
+    {
+      this.dEnsureInitialized();
+      ICommitPolicyExecutor commitPolicyExecutor = (ICommitPolicyExecutor)this.CommitPolicyExecutor.Clone();
+      commitPolicyExecutor.Initialize(this);
+      return new CloudSearchUpdateContext(this, this.deserializer, commitPolicyExecutor);
+    }
+
+
+    #region Workaround for issue 136614
+
+    public new ICloudSearchIndexSchemaBuilder SchemaBuilder
+    {
+      get { return (this as Sitecore.ContentSearch.Azure.CloudSearchProviderIndex).SchemaBuilder; }
+      set
+      {
+        var pi = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex)
+            .GetProperty("SchemaBuilder", BindingFlags.Instance | BindingFlags.Public);
+        pi.SetValue(this, value);
+      }
+    }
+
+
+    public new ISearchService SearchService
+    {
+      get { return (this as Sitecore.ContentSearch.Azure.CloudSearchProviderIndex).SearchService; }
+      set
+      {
+        var pi = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex)
+            .GetProperty("SearchService", BindingFlags.Instance | BindingFlags.Public);
+        pi.SetValue(this, value);
+      }
+    }
+
+    #endregion
+
+    protected void InitializeDelegates()
+    {
+      var t = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex);
+      var m = t.GetMethod("EnsureInitialized", BindingFlags.Instance | BindingFlags.NonPublic);
+      this.dEnsureInitialized = m.CreateDelegate(typeof(Action), this) as Action;
+    }
+  }
+}
