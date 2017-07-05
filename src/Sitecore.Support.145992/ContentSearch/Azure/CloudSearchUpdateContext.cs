SitecoreSupport/Sitecore.Support.145992

commit 13d1dd24c1e5396ed199a5be15be948b8aa84ee4

Added fix for #155653

src/Sitecore.Support.145992/ContentSearch/Azure/CloudSearchUpdateContext.cs
@@ -1,0 +1,203 @@
+﻿namespace Sitecore.Support.ContentSearch.Azure
+{
+  using Sitecore.Abstractions;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Azure;
+  using Sitecore.ContentSearch.Azure.Config;
+  using Sitecore.ContentSearch.Azure.Http;
+  using Sitecore.ContentSearch.Azure.Utils;
+  using Sitecore.ContentSearch.Diagnostics;
+  using Sitecore.ContentSearch.Linq.Common;
+  using Sitecore.ContentSearch.Sharding;
+  using Sitecore.ContentSearch.Utilities;
+  using System;
+  using System.Collections.Concurrent;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Reflection;
+  using System.Runtime.CompilerServices;
+  using System.Threading.Tasks;
+
+  public class CloudSearchUpdateContext : IProviderUpdateContext, IProviderOperationContext, IDisposable, ITrackingIndexingContext
+  {
+    private readonly ISearchResultsDeserializer deserializer;
+    private readonly ConcurrentBag<CloudSearchDocument> documents;
+    private readonly IEvent events;
+    private readonly AbstractSearchIndex index;
+    private object @lock = new object();
+    private ContextOperationStatistics statistics = new ContextOperationStatistics();
+
+    internal CloudSearchUpdateContext(AbstractSearchIndex index, ISearchResultsDeserializer deserializer, ICommitPolicyExecutor commitPolicyExecutor)
+    {
+      this.index = index;
+      this.deserializer = deserializer;
+      this.documents = new ConcurrentBag<CloudSearchDocument>();
+      this.ParallelOptions = new System.Threading.Tasks.ParallelOptions();
+      this.IsParallel = ContentSearchConfigurationSettings.IsParallelIndexingEnabled;
+      int parallelIndexingCoreLimit = ContentSearchConfigurationSettings.ParallelIndexingCoreLimit;
+      this.events = this.index.Locator.GetInstance<IEvent>();
+      if (parallelIndexingCoreLimit > 0)
+      {
+        this.ParallelOptions.MaxDegreeOfParallelism = parallelIndexingCoreLimit;
+      }
+      this.CommitPolicyExecutor = commitPolicyExecutor ?? new NullCommitPolicyExecutor();
+      this.Processed = new ConcurrentDictionary<IIndexableUniqueId, object>();
+    }
+
+    public void AddDocument(object itemToAdd, IExecutionContext executionContext)
+    {
+      if (itemToAdd == null)
+      {
+        throw new ArgumentNullException("itemToAdd");
+      }
+      IExecutionContext[] executionContexts = new IExecutionContext[] { executionContext };
+      this.AddDocument(itemToAdd, executionContexts);
+    }
+
+    public void AddDocument(object itemToAdd, params IExecutionContext[] executionContexts)
+    {
+      if (itemToAdd == null)
+      {
+        throw new ArgumentNullException("itemToAdd");
+      }
+      IDictionary<string, object> fields = itemToAdd as IDictionary<string, object>;
+      if (fields != null)
+      {
+        object @lock = this.@lock;
+        lock (@lock)
+        {
+          this.documents.Add(new CloudSearchDocument(fields, SearchAction.Upload));
+          this.CommitPolicyExecutor.IndexModified(this, fields, IndexOperation.Add);
+          this.statistics.IncrementAddCounter();
+        }
+      }
+    }
+
+    public void Commit()
+    {
+      CrawlingLog.Log.Debug($"[Index={this.index.Name}] Committing: {this.statistics}", null);
+      object[] parameters = new object[] { this.index.Name };
+      this.events.RaiseEvent("indexing:committing", parameters);
+      CloudSearchProviderIndex index = this.Index as CloudSearchProviderIndex;
+      if ((index != null) && (this.documents.Count > 0))
+      {
+        ICloudBatchBuilder builder = index.Locator.GetInstance<IFactoryWrapper>().CreateObject<ICloudBatchBuilder>("contentSearch/cloudBatchBuilder", true);
+        try
+        {
+          while (!this.documents.IsEmpty)
+          {
+            CloudSearchDocument document;
+            if (this.documents.TryTake(out document))
+            {
+              builder.AddDocument(document);
+            }
+            if (builder.IsFull || this.documents.IsEmpty)
+            {
+              ICloudBatch batch = builder.Release();
+              index.SearchService.PostDocuments(batch);
+              builder.Clear();
+            }
+          }
+        }
+        catch (Exception exception)
+        {
+          foreach (CloudSearchDocument document2 in builder)
+          {
+            this.documents.Add(document2);
+          }
+          CrawlingLog.Log.Error($"[Index={this.index.Name}] Commit failed", exception);
+          throw;
+        }
+        CrawlingLog.Log.Debug($"[Index={this.index.Name}] Committed", null);
+        object[] objArray2 = new object[] { this.index.Name };
+        this.events.RaiseEvent("indexing:committed", objArray2);
+        this.statistics = new ContextOperationStatistics();
+        this.CommitPolicyExecutor.Committed();
+      }
+    }
+
+    public void Delete(IIndexableId id)
+    {
+      if (id == null)
+      {
+        throw new ArgumentNullException("id");
+      }
+      string indexFieldName = this.Index.FieldNameTranslator.GetIndexFieldName("_id");
+      object obj2 = this.Index.Configuration.IndexFieldStorageValueFormatter.FormatValueForIndexStorage(id.Value, indexFieldName);
+      string expression = $"&$filter=({indexFieldName} eq '{obj2}')&$select={CloudSearchConfig.VirtualFields.CloudUniqueId}";
+      string textResults = (this.Index as CloudSearchProviderIndex).SearchService.Search(expression);
+      SearchResults results = this.deserializer.Deserialize(textResults);
+      this.Delete(results.Documents.First<IndexedDocument>().AzureUniqueId);
+    }
+
+    public void Delete(IIndexableUniqueId id)
+    {
+      if (id == null)
+      {
+        throw new ArgumentNullException("id");
+      }
+
+      var mi = typeof(CloudIndexParser).GetMethod("HashUniqueId", BindingFlags.Static | BindingFlags.Public);
+      this.Delete((string)mi.Invoke(null, new object[] { id.Value.ToString() }));
+    }
+
+    private void Delete(string cloudUniqueId)
+    {
+      ConcurrentDictionary<string, object> fields = new ConcurrentDictionary<string, object>();
+      fields.TryAdd(CloudSearchConfig.VirtualFields.CloudUniqueId, cloudUniqueId);
+      this.documents.Add(new CloudSearchDocument(fields, SearchAction.Delete));
+      object @lock = this.@lock;
+      lock (@lock)
+      {
+        this.CommitPolicyExecutor.IndexModified(this, cloudUniqueId, IndexOperation.Delete);
+        this.statistics.IncrementDeleteUniqueCounter();
+      }
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public void Optimize()
+    {
+    }
+
+    public void UpdateDocument(object itemToUpdate, object criteriaForUpdate, IExecutionContext executionContext)
+    {
+      IExecutionContext[] executionContexts = new IExecutionContext[] { executionContext };
+      this.UpdateDocument(itemToUpdate, criteriaForUpdate, executionContexts);
+    }
+
+    public void UpdateDocument(object itemToUpdate, object criteriaForUpdate, params IExecutionContext[] executionContexts)
+    {
+      if (itemToUpdate == null)
+      {
+        throw new ArgumentNullException("itemToUpdate");
+      }
+      IDictionary<string, object> fields = itemToUpdate as IDictionary<string, object>;
+      if (fields != null)
+      {
+        object @lock = this.@lock;
+        lock (@lock)
+        {
+          this.documents.Add(new CloudSearchDocument(fields, SearchAction.Upload));
+          this.CommitPolicyExecutor.IndexModified(this, fields, IndexOperation.Add);
+          this.statistics.IncrementUpdateCounter();
+        }
+      }
+    }
+
+    public ICommitPolicyExecutor CommitPolicyExecutor { get; private set; }
+
+    public ISearchIndex Index =>
+        this.index;
+
+    public bool IsParallel { get; private set; }
+
+    public System.Threading.Tasks.ParallelOptions ParallelOptions { get; private set; }
+
+    public ConcurrentDictionary<IIndexableUniqueId, object> Processed { get; set; }
+
+    public IEnumerable<Shard> ShardsWithPendingChanges { get; private set; }
+  }
+}

src/Sitecore.Support.145992/CloudSearchProviderIndex.cs
@@ -1,75 +1,0 @@
-﻿
-namespace Sitecore.Support.ContentSearch.Azure
-{
-    using System;
-    using System.Reflection;
-    using Sitecore.ContentSearch;
-    using Sitecore.ContentSearch.Azure.Http;
-    using Sitecore.ContentSearch.Azure.Schema;
-    using Sitecore.ContentSearch.Maintenance;
-    using Sitecore.ContentSearch.Security;
-
-    public class CloudSearchProviderIndex : Sitecore.ContentSearch.Azure.CloudSearchProviderIndex
-    {
-        private Action dEnsureInitialized;
-
-        public CloudSearchProviderIndex(string name, string connectionStringName, string totalParallelServices,
-            IIndexPropertyStore propertyStore) : base(name, connectionStringName, totalParallelServices, propertyStore)
-        {
-            this.InitializeDelegates();
-        }
-
-        public CloudSearchProviderIndex(string name, string connectionStringName, string totalParallelServices,
-            IIndexPropertyStore propertyStore, string @group)
-            : base(name, connectionStringName, totalParallelServices, propertyStore, @group)
-        {
-            this.InitializeDelegates();
-        }
-
-        public override IIndexOperations Operations
-        {
-            get { return new Sitecore.Support.ContentSearch.Azure.CloudSearchIndexOperations(this); }
-        }
-
-        public override IProviderSearchContext CreateSearchContext(SearchSecurityOptions options)
-        {
-            this.dEnsureInitialized();
-            return new Sitecore.Support.ContentSearch.Azure.CloudSearchSearchContext(this, options);
-        }
-
-
-        #region Workaround for issue 136614
-
-        public new ICloudSearchIndexSchemaBuilder SchemaBuilder
-        {
-            get { return (this as Sitecore.ContentSearch.Azure.CloudSearchProviderIndex).SchemaBuilder; }
-            set
-            {
-                var pi = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex)
-                    .GetProperty("SchemaBuilder", BindingFlags.Instance | BindingFlags.Public);
-                pi.SetValue(this, value);
-            }
-        }
-
-
-        public new ISearchService SearchService
-        {
-            get { return (this as Sitecore.ContentSearch.Azure.CloudSearchProviderIndex).SearchService; }
-            set
-            {
-                var pi = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex)
-                    .GetProperty("SearchService", BindingFlags.Instance | BindingFlags.Public);
-                pi.SetValue(this, value);
-            }
-        }
-
-        #endregion
-
-        protected void InitializeDelegates()
-        {
-            var t = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex);
-            var m = t.GetMethod("EnsureInitialized", BindingFlags.Instance | BindingFlags.NonPublic);
-            this.dEnsureInitialized = m.CreateDelegate(typeof(Action), this) as Action;
-        }
-    }
-}

src/Sitecore.Support.145992/ContentSearch/Azure/CloudSearchProviderIndex.cs
@@ -1,0 +1,91 @@
+﻿
+namespace Sitecore.Support.ContentSearch.Azure
+{
+  using System;
+  using System.Reflection;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Azure.Http;
+  using Sitecore.ContentSearch.Azure.Schema;
+  using Sitecore.ContentSearch.Maintenance;
+  using Sitecore.ContentSearch.Security;
+
+  public class CloudSearchProviderIndex : Sitecore.ContentSearch.Azure.CloudSearchProviderIndex
+  {
+    private ISearchResultsDeserializer deserializer;
+    private Action dEnsureInitialized;
+
+    public CloudSearchProviderIndex(string name, string connectionStringName, string totalParallelServices,
+        IIndexPropertyStore propertyStore) : base(name, connectionStringName, totalParallelServices, propertyStore)
+    {
+      this.InitializeDelegates();
+    }
+
+    public CloudSearchProviderIndex(string name, string connectionStringName, string totalParallelServices,
+        IIndexPropertyStore propertyStore, string @group)
+        : base(name, connectionStringName, totalParallelServices, propertyStore, @group)
+    {
+      this.InitializeDelegates();
+    }
+
+    public override void Initialize()
+    {
+      base.Initialize();
+      var d = typeof(CloudSearchProviderIndex).BaseType.GetField("deserializer", BindingFlags.Instance | BindingFlags.NonPublic);
+      this.deserializer = d as ISearchResultsDeserializer;
+    }
+
+    public override IIndexOperations Operations
+    {
+      get { return new Sitecore.Support.ContentSearch.Azure.CloudSearchIndexOperations(this); }
+    }
+
+    public override IProviderSearchContext CreateSearchContext(SearchSecurityOptions options)
+    {
+      this.dEnsureInitialized();
+      return new Sitecore.Support.ContentSearch.Azure.CloudSearchSearchContext(this, options);
+    }
+
+    public override IProviderUpdateContext CreateUpdateContext()
+    {
+      this.dEnsureInitialized();
+      ICommitPolicyExecutor commitPolicyExecutor = (ICommitPolicyExecutor)this.CommitPolicyExecutor.Clone();
+      commitPolicyExecutor.Initialize(this);
+      return new CloudSearchUpdateContext(this, this.deserializer, commitPolicyExecutor);
+    }
+
+
+    #region Workaround for issue 136614
+
+    public new ICloudSearchIndexSchemaBuilder SchemaBuilder
+    {
+      get { return (this as Sitecore.ContentSearch.Azure.CloudSearchProviderIndex).SchemaBuilder; }
+      set
+      {
+        var pi = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex)
+            .GetProperty("SchemaBuilder", BindingFlags.Instance | BindingFlags.Public);
+        pi.SetValue(this, value);
+      }
+    }
+
+
+    public new ISearchService SearchService
+    {
+      get { return (this as Sitecore.ContentSearch.Azure.CloudSearchProviderIndex).SearchService; }
+      set
+      {
+        var pi = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex)
+            .GetProperty("SearchService", BindingFlags.Instance | BindingFlags.Public);
+        pi.SetValue(this, value);
+      }
+    }
+
+    #endregion
+
+    protected void InitializeDelegates()
+    {
+      var t = typeof(Sitecore.ContentSearch.Azure.CloudSearchProviderIndex);
+      var m = t.GetMethod("EnsureInitialized", BindingFlags.Instance | BindingFlags.NonPublic);
+      this.dEnsureInitialized = m.CreateDelegate(typeof(Action), this) as Action;
+    }
+  }
+}

commit 4cd6f3d889309540d495c6b516d51975a8047723

Fix issue introduced by 13d1dd24c1e5396ed199a5be15be948b8aa84ee4.

src/Sitecore.Support.145992/ContentSearch/Azure/CloudSearchUpdateContext.cs
@@ -16,6 +16,7 @@
   using System.Linq;
   using System.Reflection;
   using Sitecore.Diagnostics;
+  using Sitecore.Support.ContentSearch.Azure.Utils;
 
   public class CloudSearchUpdateContext : IProviderUpdateContext, IProviderOperationContext, IDisposable, ITrackingIndexingContext
   {
@@ -173,8 +174,7 @@
         throw new ArgumentNullException("id");
       }
 
-      var mi = typeof(CloudIndexParser).GetMethod("HashUniqueId", BindingFlags.Static | BindingFlags.Public);
-      this.Delete((string)mi.Invoke(null, new object[] { id.Value.ToString() }));
+      this.Delete(PublicCloudIndexParser.HashUniqueId(id.Value.ToString()));
     }
 
     private static readonly MethodInfo DeleteMethodInfo =